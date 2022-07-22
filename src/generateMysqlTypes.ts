5d168ab77375b420a944b3336eb427218e7d39e7
Corey Armstrong <coreyarms@gmail.com>
2022-07-22 07:07:05 -0400

    0.0.8

--- src/generateMysqlTypes.ts
+++ src/generateMysqlTypes.ts
@@ -83,15 +83,15 @@ export const generateMysqlTypes = async (config: GenerateMysqlTypesConfig) => {
     // output the columns and types
     for (const column of columns) {
       let columnDataType = getColumnDataType(column.DATA_TYPE, column.COLUMN_TYPE);
-      
-      const columnOverride = config.overrides?.find((override) => override.tableName === table && override.columnName === column.COLUMN_NAME);
+
+      const columnOverride = config.overrides?.find(
+        (override) => override.tableName === table && override.columnName === column.COLUMN_NAME,
+      );
       if (columnOverride) {
         columnDataType = getColumnDataType(column.DATA_TYPE, columnOverride.columnType);
       }
 
-      outputTypeFileStream.write(
-        `  ${column.COLUMN_NAME}: ${columnDataType};\n`,
-      );
+      outputTypeFileStream.write(`  ${column.COLUMN_NAME}: ${columnDataType};\n`);
     }
     outputTypeFileStream.write('}\n');
 
