0ad54cb7cf61040bdfcf7b8e5cd18e0e06e4151b
Corey Armstrong <coreyarms@gmail.com>
2022-07-23 19:51:23 -0400

    align asterisks in jsdoc warning header

--- src/warningHeader.ts
+++ src/warningHeader.ts
@@ -1,6 +1,7 @@
 export const warningHeader = `/**
-* This file is auto-generated and should not be edited.
-* It will be overwritten the next time types are generated.
-*/
+ * This file is auto-generated and should not be edited.
+ * It will be overwritten the next time types are generated.
+ */
 
 `;
+ 
