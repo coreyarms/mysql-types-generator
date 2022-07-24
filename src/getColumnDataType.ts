d8b5ba778a417181eef7a8e870c2f1bb2d7043d5
Corey Armstrong <coreyarms@gmail.com>
2022-07-23 23:47:59 -0400

    bugfix

--- src/getColumnDataType.ts
+++ src/getColumnDataType.ts
@@ -1,4 +1,4 @@
-export const getColumnDataType = (dataType: string|null, columnType: string): string => {
+export const getColumnDataType = (dataType: string|null, columnType: string, tinyintIsBoolean: boolean): string => {
   if (columnType === null) {
     throw new Error('The DATA_TYPE field in information_schema should never be null. This may be a bug');
   }
